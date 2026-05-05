9925d33dc0ebc10c38029b6dcfff5b2c23eee997
Clairo <clairevigneron@imac-de-clairo-2.home>
2026-05-05 12:20:57 +0200

    OAuth Instagram : ajout scope instagram_business_manage_messages

--- functions/api/instagram-auth.js
+++ functions/api/instagram-auth.js
@@ -7,7 +7,7 @@ export async function onRequestGet({ request, env }) {
   const REDIRECT = 'https://claire-cm-site.pages.dev/api/instagram-callback';
   const state    = btoa(JSON.stringify({ clientId, from }));
 
-  const authUrl = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${APP_ID}&redirect_uri=${REDIRECT}&response_type=code&scope=instagram_business_basic,instagram_business_manage_insights,instagram_business_content_publish,instagram_business_manage_comments&state=${encodeURIComponent(state)}`;
+  const authUrl = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${APP_ID}&redirect_uri=${REDIRECT}&response_type=code&scope=instagram_business_basic,instagram_business_manage_insights,instagram_business_content_publish,instagram_business_manage_comments,instagram_business_manage_messages&state=${encodeURIComponent(state)}`;
 
   return Response.redirect(authUrl, 302);
 }
